1887755c5b48c563e8aacc26149f389e2d4687e4
Yash Mane <141533114+Ayankouji@users.noreply.github.com>
2025-09-22 19:33:47 +0530

    Update home.py

--- home.py
+++ home.py
@@ -7,3 +7,4 @@ def app():
     st.subheader('Summary')
 
     st.text_area(label='#',value="""1)Here We Analyzing & Predicting Health Diseases Such As Respiratory Diseases - Cardio Health Which is Moslty Found In Number of peoples around the world by analyzing the data\n2)we can predict the health risk causing to peoples & also helps to prevent the risk of causing such diseases in the population """)
+    st.subheader("Created by Yash Mane ")
